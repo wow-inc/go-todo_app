0edb38863450a27fbbd4d8b811448053a6c5e84e
Keisue <keisuke@wowinc.app>
2022-05-19 00:00:41 +0900

    create GetTodos

--- main.go
+++ main.go
@@ -42,7 +42,17 @@ func main() {
 		user.CreateTodo("First Todo")
 	*/
 
-	t, _ := models.GetTodo(1)
+	/*
+		t, _ := models.GetTodo(1)
+		fmt.Println(t)
+	*/
 
-	fmt.Println(t)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Second Todo")
+
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
+
 }

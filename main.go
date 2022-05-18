0d4425c694fa46cb34afc1959de35664f212fa87
Keisue <keisuke@wowinc.app>
2022-05-18 23:24:56 +0900

    Create Todo

--- main.go
+++ main.go
@@ -6,36 +6,34 @@ import (
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-	// fmt.Println(config.Config.SQLDriver)
-
-	// log.Println("test")
 
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@gmail.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	u, _ := models.GetUser(1)
-
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@gmail.com"
+	u.PassWord = "testtest"
 	fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
+	u.CreateUser()
 
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	/*
+		u, _ := models.GetUser(1)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+		fmt.Println(u)
 
+		u.Name = "Test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+
+		u.DeleteUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+	*/
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }

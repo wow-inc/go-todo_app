bcbb4aa7c4587edb6b674f131448e724cda47e14
Keisue <keisuke@wowinc.app>
2022-05-19 00:14:00 +0900

    create GetTodosByUser

--- main.go
+++ main.go
@@ -11,13 +11,12 @@ func main() {
 
 	/*
 		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@gmail.com"
+		u.Name = "test2"
+		u.Email = "test2@gmail.com"
 		u.PassWord = "testtest"
 		fmt.Println(u)
 
 		u.CreateUser()
-
 	*/
 
 	/*
@@ -47,10 +46,21 @@ func main() {
 		fmt.Println(t)
 	*/
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
+	/*
+		user, _ := models.GetUser(3)
+		user.CreateTodo("Third Todo")
+	*/
 
-	todos, _ := models.GetTodos()
+	/*
+		todos, _ := models.GetTodos()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	user3, _ := models.GetUser(3)
+	todos, _ := user3.GetTodosByUser()
+
 	for _, v := range todos {
 		fmt.Println(v)
 	}

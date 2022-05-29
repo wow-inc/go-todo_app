e8d0f4e9ed303a89bffd7a2a1ad561a77526eae9
Keisue <keisuke@wowinc.app>
2022-05-30 07:46:10 +0900

    route_auth.go SignUp

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -7,19 +7,24 @@ import (
 	"todo_app_go/config"
 )
 
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		fmt.Println(file)
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	fmt.Println(files)
+
+	templates := template.Must(template.ParseFiles(files...))
+	fmt.Println(templates)
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
-}
-
-func GeneratHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
-	var files []string
-	for _, file := range filenames {
-		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
-	}
-
-	templates := template.Must(template.ParseFiles(files...))
-	templates.ExecuteTemplate(w, "layout", data)
 }

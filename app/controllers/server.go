a73182d5d9e518627ea3ecd46c6ed65463d0b21b
Keisue <keisuke@wowinc.app>
2022-05-31 13:07:33 +0900

    route_main.go CreateTodo

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -43,5 +43,7 @@ func StartMainServer() error {
 	http.HandleFunc("/authenticate", authenticate)
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/todos/new", todoNew)
+	http.HandleFunc("/todos/save", todoSave)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
